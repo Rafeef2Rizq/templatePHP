3f280a805bb59cbccf826065cde5797129ca4291
Rafeef2Rizq <rfyfrzq7@gmail.com>
2025-11-19 18:12:52 +0200

    inital template

---
+++ src/Framework/App.php
@@ -0,0 +1,26 @@
+<?php
+//to connect fraamework tools ander single apps
+declare(strict_types=1);
+
+namespace Framework;
+
+use Framework\Router;
+
+class App
+{
+   private Router $router;
+   public function  __construct()
+   {
+      $this->router = new Router();
+   }
+   public function run()
+   {
+      $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+      $method = $_SERVER['REQUEST_METHOD'];
+      $this->router->dispatch($path, $method);
+   }
+   public function get(string $path, array $controller)
+   {
+      $this->router->add('get', $path, $controller);
+   }
+}
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rafeefrezeq/phpiggy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '47cdbe6d1fb009d72f48992353d05ca9750ad87c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rafeefrezeq/phpiggy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '47cdbe6d1fb009d72f48992353d05ca9750ad87c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
